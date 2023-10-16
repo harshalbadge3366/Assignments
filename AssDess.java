9ca7559eb243eb420d627707a2ccec40c121b7c8
harshalbadge3366 <98678820+harshalbadge3366@users.noreply.github.com>
2023-10-16 09:13:00 +0530

    Fourth Problem
    Fourth Problem

---
+++ AssDess.java
@@ -0,0 +1,13 @@
+package Assignment;
+
+public class AssDess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AssenDessen ob6 = new AssenDessen();
+		  int[] arr = {3, 2, 4, 1, 10, 30, 40, 20};
+	       int n = arr.length;
+	       ob6.printOrder(arr, n);
+	
+}
+}
